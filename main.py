d28b549438a251f5138beedab83491dde5eaa144
vincentsheehan <vin.twentywo@gmail.com>
2022-07-31 23:47:27 +0100

    Update comments.

--- main.py
+++ main.py
@@ -10,18 +10,20 @@ pd.options.display.width = 0
 if __name__ == '__main__':
     df_nhl = pd.read_csv("0001-7461_corrected.csv")
 
+    # Sort dataframe by Points ('P') and Games Played ('GP') columns.
     df_nhl = df_nhl.sort_values(by=['P', 'GP'], ascending=False)
 
+    # Summarise dataset.
     print(df_nhl.head())
     print(df_nhl.tail())
     print(df_nhl.describe())
     print(df_nhl.info())
 
+    # Define features of interest.
     games_played = df_nhl['GP']
-    print(games_played)
     points = df_nhl['P']
-    print(points)
 
+    # Explore dataset with scatter plot of points vs. games played.
     plt.plot(games_played, points, 'o', alpha=0.5)
     plt.title('Career Points vs. Games Played - Regular Season')
     plt.xlabel('Games Played')

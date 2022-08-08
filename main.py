1e8c744be1184f8a00c571629690758d9b0825ca
vincentsheehan <vin.twentywo@gmail.com>
2022-08-08 21:37:09 +0100

    Add boxplot of points for all positions combined.

--- main.py
+++ main.py
@@ -190,19 +190,26 @@ if __name__ == '__main__':
     #plt.show()
 
     # Set up figure for multiple plots.
-    f, axs = plt.subplots(1, 2)
+    f, axs = plt.subplots(1, 3)
     f.suptitle('Points per Position')
 
     # Box plot of P vs. Pos, using sns.
-    sns.boxplot(x="Pos", y="P", data=df_nhl, ax=axs[0])
+    sns.boxplot(data=df_nhl["P"], ax=axs[0])
     axs[0].grid(True, axis='both')
     axs[0].set(xlabel="Position")
+    axs[0].set_xticklabels(["All"])
     axs[0].set(ylabel="Points")
 
     # Box plot of P vs. Pos, with further categorisation on S/C.
-    sns.boxplot(x="Pos", y="P", hue="S/C", data=df_nhl, ax=axs[1])
+    sns.boxplot(x="Pos", y="P", data=df_nhl, ax=axs[1])
     axs[1].grid(True, axis='both')
     axs[1].set(xlabel="Position")
     axs[1].set(ylabel="Points")
+
+    # Box plot of P vs. Pos, with further categorisation on S/C.
+    sns.boxplot(x="Pos", y="P", hue="S/C", data=df_nhl, ax=axs[2])
+    axs[2].grid(True, axis='both')
+    axs[2].set(xlabel="Position")
+    axs[2].set(ylabel="Points")
 
     plt.show()
